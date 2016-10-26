7fd0d7d31fa288c74e616bd415812c2ba50bc44c
charroux <benoit.charroux@free.fr>
2016-10-26 12:19:30 +0200

    ajout converter json

--- src/main/java/myPackage/MyService.java
+++ src/main/java/myPackage/MyService.java
@@ -2,9 +2,9 @@ package myPackage;
 
 public class MyService {
 
-	public MyClasse myMethod(String s){
+	public String myMethod(String s){
 		System.out.println("Je suis myMethod de MyService. Je recois " + s + " que je retourne");
-		return new MyClasse(s, 5);
+		return s;
 	}
 
 }
