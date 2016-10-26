1df972c3b21fd65f683d7213e65bcb4e19f1ac05
charroux <benoit.charroux@free.fr>
2016-10-26 10:22:16 +0200

    adding a service activator

---
+++ src/main/java/myPackage/MyService.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class MyService {
+
+	public String myMethod(String s){
+		System.out.println("Je suis myMethod de MyService. Je recois " + s + " que je retourne");
+		return s;
+	}
+
+}
