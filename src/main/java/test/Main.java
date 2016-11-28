a817928e0ca9e0b97b4cd581671631dbdf65d525
charroux <benoit.charroux@free.fr>
2016-11-28 17:35:07 +0100

    adding ws

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("javaScriptService.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 	}
 
