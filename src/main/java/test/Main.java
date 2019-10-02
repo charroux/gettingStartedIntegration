b84cc8d9b46a6faa50810e6bb23c99ee9d92fc41
orchaland <orchaland@gmail.com>
2019-10-02 16:51:17 +0200

    service activator

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("serviceActivator.xml");
 
 	}
 
