6d787a24e84cd5e3e613169a9ddb3b0159517dce
orchaland <orchaland@gmail.com>
2020-11-10 14:40:50 +0100

    rputing

--- src/main/java/test/AccountService.java
+++ src/main/java/test/AccountService.java
@@ -4,8 +4,13 @@ public class AccountService {
 	
 	public MyAccount myService(MyAccount myAccount){
 		System.out.println(myAccount);
-		myAccount.setAmount(myAccount.getAmount() + 200);
-		return myAccount;
+		MyAccount ma = new MyAccount();
+		ma.setAmount(myAccount.getAmount() + 200);
+		ma.setName(myAccount.getName());
+		ma.setNumber(myAccount.getNumber());
+		return ma;
+		//myAccount.setAmount(myAccount.getAmount() + 200);
+		//return myAccount;
 	}
 
 }
