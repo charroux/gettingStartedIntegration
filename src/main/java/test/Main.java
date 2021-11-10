b537994b7f5d5ef15f21f9c6c00f1e1ad0066aa3
orchaland <orchaland@gmail.com>
2021-11-10 11:28:10 +0100

    router

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -10,11 +10,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		// ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 		//ApplicationContext ctx = new ClassPathXmlApplicationContext("serviceActivator.xml");
 
-		// ApplicationContext ctx = new ClassPathXmlApplicationContext("routing.xml");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("routing.xml");
 
 		/*
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("javaScriptService.xml");
