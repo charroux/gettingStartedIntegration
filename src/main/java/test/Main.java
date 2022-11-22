92a3d76a57d5db2d4878f7d538278b8c0b919612
charroux <benoit.charroux@gmail.com>
2022-11-22 17:43:45 +0100

    init

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -10,39 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		// ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-
-		//ApplicationContext ctx = new ClassPathXmlApplicationContext("serviceActivator.xml");
-
-		//ApplicationContext ctx = new ClassPathXmlApplicationContext("routing.xml");
-
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("aggragate.xml");
-
-		/*
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("javaScriptService.xml");
-
-		MyGateway myGateway= (MyGateway)ctx.getBean("gateWayService");
-		myGateway.method("Salut !");
-		*/
-
-
-		/*ApplicationContext ctx = new ClassPathXmlApplicationContext("webServiceCalling.xml");
-
-		MyGateway myGateway= (MyGateway)ctx.getBean("gateWayService");
-		myGateway.method("Salut !");*/
-
-
-
-		/*ApplicationContext ctx = new ClassPathXmlApplicationContext("database.xml");
-
-		PersonService personService = ctx.getBean(PersonService.class);
-		
-		Person person = new Person();
-		person.setName("Tintin");
-		person = personService.createPerson(person);
-
-		System.out.println(person);*/
-
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 
 	}
 
